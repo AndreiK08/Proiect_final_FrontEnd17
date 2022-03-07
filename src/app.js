9357a9ab69607b3b7f6fac8435f8b2dd50874de4
Andrei Cimpean <andreicimpean08@gmail.com>
2022-03-07 21:50:49 +0200

    Update 07.03

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+window.addEventListener('load', async () => {
+	const productsURL = 'https://62146cca89fad53b1f136ccd.mockapi.io/products';
+	const result = await fetch(productsURL);
+	const products = await result.json();
+
+	const productContainer = document.querySelector('.products-container');
+
+	const cards = products
+		.map(
+			(product) =>
+				`<div class="card" style="width: 18rem;">
+               <div class="card-body">
+                  <h5 class="card-title">${product.name}</h5>
+                  <p class="card-text">${product.price}</p>
+                  <a href="details.html?product-id=${product.id}" class="btn btn-primary">Details</a>
+               </div>
+            </div>`
+		)
+		.join('');
+
+	productContainer.innerHTML = cards;
 });
---
+++ src/details.js
@@ -0,0 +1,64 @@
+// export function showDetails() {
+// 	console.log('details');
+// }
+
+// export function showDetails2() {
+// 	console.log('details2');
+// }
+
+window.addEventListener('load', async () => {
+	let searchParamString = window.location.search;
+
+	const searchParam = new URLSearchParams(searchParamString);
+	const productId = searchParam.get('product-id');
+
+	const productURL = `https://62146cca89fad53b1f136ccd.mockapi.io/products/${productId}`;
+	const result = await fetch(productURL);
+	const product = await result.json();
+
+	const productCard = `
+		<div class="card">
+			<div class="card-header">
+	  			Product Details
+			</div>
+			<div class="card-body">
+	  			<h5 class="card-title">${product.name}</h5>
+	  			<p class="card-text">${product.description}</p>
+	  			<p class="card-text">${product.price}</p>
+	  			<button data-product-id=${product.id} class="add-to-cart btn btn-primary">Add to cart</button>
+			</div>
+ 		</div>`;
+
+	document.querySelector('.product-details').innerHTML = productCard;
+});
+
+document.querySelector('.product-details').addEventListener('click', addToCart);
+async function addToCart(event) {
+	const addToCartBtn = event.target;
+	let productId = addToCartBtn.getAttribute('data-product-id');
+
+	const productURL = `https://61e0713863f8fc0017618791.mockapi.io/products/${productId}`;
+	const result = await fetch(productURL);
+	const product = await result.json();
+
+	let cart = [];
+	if (localStorage.getItem('cart') == null) {
+		cart.push({ ...product, noOfProducts: 1 });
+	} else {
+		cart = JSON.parse(localStorage.getItem('cart'));
+		const productInCart = cart.find(
+			(productFromCart) => productFromCart.id == product.id
+		);
+		if (productInCart != undefined) {
+			productInCart.noOfProducts++;
+			console.log('Produsul exista in cos');
+		} else {
+			const productToBeAddedInCart = { ...product, noOfProducts: 1 };
+			cart.push(productToBeAddedInCart);
+			console.log('Produsul a fost adaugat prima oara in cos');
+		}
+	}
+
+	console.log(cart);
+	if (cart.length > 0) localStorage.setItem('cart', JSON.stringify(cart));
+}
